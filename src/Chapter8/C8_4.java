a860ac69523d56770db4b86b8090dfc9695c3cf2
BrianCharter <bc0928626@RK03-B1235-14.francistuttle.edu>
2018-01-26 14:30:15 -0600

    Chapter 7 and 8

--- src/Chapter8/C8_4.java
+++ src/Chapter8/C8_4.java
@@ -1,44 +1,40 @@
 package Chapter8;
 
 /**
- * Outputs a chart showing number of hours worked by each employee each day and the weekly amount of hours worked
+ * Outputs a chart showing number of hours worked by each employee each day and
+ * the weekly amount of hours worked
  *
  * @author Brian Charter
  */
 public class C8_4 {
-       
+
     /**
      * Main Method
      *
      * @param args arguments from command line prompt
      */
-    public static void main(String[] args)
-    {
-        int[][] hours = {{2,4,3,4,5,8,8},
-                         {7,3,4,3,3,4,4},
-                         {3,3,4,3,3,2,2},
-                         {9,3,4,7,3,4,1},
-                         {3,5,4,3,6,3,8},
-                         {3,4,4,6,3,4,4},
-                         {3,7,4,8,3,8,4},
-                         {6,3,5,9,2,7,9}};
-        
+    public static void main(String[] args) {
+        int[][] hours = {{2, 4, 3, 4, 5, 8, 8},
+        {7, 3, 4, 3, 3, 4, 4},
+        {3, 3, 4, 3, 3, 2, 2},
+        {9, 3, 4, 7, 3, 4, 1},
+        {3, 5, 4, 3, 6, 3, 8},
+        {3, 4, 4, 6, 3, 4, 4},
+        {3, 7, 4, 8, 3, 8, 4},
+        {6, 3, 5, 9, 2, 7, 9}};
+
         System.out.println("            Su   M   T   W  Th   F  Sa   Total");
-        
-        for(int x = 0; x < hours.length; x++)
-        {
+
+        for (int x = 0; x < hours.length; x++) {
             System.out.print("Employee " + x);
             int total = 0;
-            for(int y = 0; y < hours[x].length; y++)
-            {
+            for (int y = 0; y < hours[x].length; y++) {
                 System.out.print("   " + hours[x][y]);
                 total += hours[x][y];
             }
             System.out.println("   " + total);
         }
-        
-        
-        
+
     }
-    
+
 }
--- src/Chapter8/P8.java
+++ src/Chapter8/P8.java
@@ -1,86 +1,92 @@
 package Chapter8;
+
 import java.util.Scanner;
 
 /**
- * Accepts sales for 4 different salesman and outputs the total amount of sales per day
+ * Accepts sales for 4 different salesman and outputs the total amount of sales
+ * per day
  *
  * @author Brian Charter
  */
 public class P8 {
-    
-     /**
+
+    /**
      * Main Method
      *
      * @param args arguments from command line prompt
      */
-    public static void main(String[] args)
-    {
-        
+    public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-        
+
         String[] salesmen = {"A", "B", "C", "D"};
         double[][] salesArray = new double[4][5];
         boolean done = true;
-        
+
         char ID = ' ';
-        
-        while(done)
-        {
+
+        while (done) {
             int index = 0, index2 = 0;
             System.out.print("Enter the salesman ID as A, B, C, or D: ");
-            switch(input.next())
-            {
-                case "A": index = 0;
+            switch (input.next()) {
+                case "A":
+                    index = 0;
                     break;
-                case "B": index = 1;
+                case "B":
+                    index = 1;
                     break;
-                case "C": index = 2;
+                case "C":
+                    index = 2;
                     break;
-                case "D": index = 3;
+                case "D":
+                    index = 3;
                     break;
             }
             System.out.print("Enter the day as M, T, W, H, or F: ");
-            switch(input.next())
-            {
-                case "M": index2 = 0;
+            switch (input.next()) {
+                case "M":
+                    index2 = 0;
                     break;
-                case "T": index2 = 1;
+                case "T":
+                    index2 = 1;
                     break;
-                case "W": index2 = 2;
+                case "W":
+                    index2 = 2;
                     break;
-                case "H": index2 = 3;
+                case "H":
+                    index2 = 3;
                     break;
-                case "F": index2 = 4;
+                case "F":
+                    index2 = 4;
                     break;
             }
-            
+
             System.out.print("Enter the amount of the sale: ");
             salesArray[index][index2] += input.nextDouble();
-            
+
             System.out.print("More data? Enter Y for more or N to stop. ");
-            
-            switch(input.next())
-            {
-                case "Y":   done = true;
+
+            switch (input.next()) {
+                case "Y":
+                    done = true;
                     break;
-                case "N": done = false;
+                case "N":
+                    done = false;
                     break;
 
             }
         }
-        
+
         System.out.println("Salesman   M     T     W    H     AF");
-        
-        for(int x = 0; x < salesArray.length; x++)
-        {
+
+        for (int x = 0; x < salesArray.length; x++) {
             System.out.print("       " + salesmen[x]);
-            for(int y = 0; y < salesArray[x].length; y++)
-            {
+            for (int y = 0; y < salesArray[x].length; y++) {
                 System.out.print("   " + salesArray[x][y]);
             }
             System.out.println();
         }
-        
+
     }
-    
+
 }
